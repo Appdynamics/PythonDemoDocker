1d3723f1287199bacb28c341f907a7100df5bbc2
Eric Johanson <eric.johanson@osxltejohanson.corp.appdynamics.com>
2016-03-03 11:28:01 -0800

    Added if not exists to create statement

--- Docker/app/src/pythonapp.py
+++ Docker/app/src/pythonapp.py
@@ -120,7 +120,7 @@ def showSignUp():
     cursor.execute("USE AppDynamics")
     cursor.execute("DROP TABLE IF EXISTS tbl_user")
     cursor.execute("FLUSH TABLES")
-    cursor.execute("CREATE TABLE tbl_user(Id INTEGER PRIMARY KEY, Name VARCHAR(20), Email VARCHAR(20))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS tbl_user(Id INTEGER PRIMARY KEY, Name VARCHAR(20), Email VARCHAR(20))")
     booleanvalue = checkAccount()
     conn.commit()
     cursor.close()
