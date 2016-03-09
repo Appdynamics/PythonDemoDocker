621c6a1cdb984c4184e330027b42e4f55cb87452
Jeff Morgan <jeff.morgan@appdynamics.com>
2016-03-09 22:05:39 +0000

    Fixed postgres startup issue

--- postgres/startup.sh
+++ postgres/startup.sh
@@ -4,5 +4,5 @@
 /etc/init.d/ssh start
 
 # Start Postgres
-su - postgres -c 'service postgresql start'
+su - postgres -c 'service postgresql restart'
 tail -f /var/log/postgresql/postgresql-9.3-main.log
